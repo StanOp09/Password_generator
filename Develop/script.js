4b3ed01948b0d34f7a231a4c5fa3da55da4c996b
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-06-19 16:04:22 -0600

    added updated script.js and README.md files

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,216 @@
+// Define character sets
+var passwordChoices = {
+  UpperCase: [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ],
+  LowerCase: [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ],
+  numbers: [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+  ],
+  special: [
+    ' ', 
+    '!', 
+    '#', 
+    '$', 
+    '%', 
+    '&',
+    "'", 
+    '(', 
+    ')', 
+    '*', 
+    '+', 
+    ',',
+    '-', 
+    '.', 
+    '/', 
+    ':', 
+    ';', 
+    '<',
+    '=', 
+    '>', 
+    '?', 
+    '@', 
+    '[', 
+    ']',
+    '^', 
+    '_', 
+    '`', 
+    '{', 
+    '|', 
+    '}',
+    '~'
+  ]
+  
+}
+
+// Function to randomly select numbers includng the maximum number
+function randomNumberPicker(max) {
+  // Generate a random number between 0 and max (inclusive)
+  var randomNumber = Math.floor(Math.random() * (max));
+
+  // Return the random number
+  return randomNumber;
+}
+
+// Create a fxn to select randomly from the set of available characters
+function randomPicker(chars) {
+  if (chars && chars.length) {
+    var randomIndex = randomNumberPicker(chars.length);
+    return chars[randomIndex];
+  } else {
+    return console.log(`Invalid!`)
+  }
+}
+
+// Function to shuffle available characters
+function characterShuffle(input) {
+  var charsArray = input.split("");
+  for (var i=0; i<charsArray.length-1; i++) {
+    var randomIndex = randomNumberPicker(charsArray.length);
+    var randomvalue = input[i]
+    input[i] = input[randomIndex];
+    input[randomIndex] = randomvalue
+    }
+    return charsArray.join("");
+}
+
+function getSuggestions() {
+  // ask user for length of password
+  var passwordLength = prompt("Choose the number of characters to use to generate password");
+  // convert response to number
+  passwordLength = parseInt(passwordLength)
+  // validate choice
+  if (isNaN(passwordLength) || passwordLength <8 || passwordLength > 128) {
+    alert("Invalid choice. Only numbers between 8 and 128 allowed");}
+  else
+    alert("Click OK to confirm");
+  
+  var useUppercase = confirm("Include uppercase letters in the password?");
+  var useLowercase = confirm("Include lowercase letters in the password?");
+  var useNumbers = confirm("Include numbers in the password?");
+  var useSpecial = confirm("Include special characters in the password?");
+
+  return {
+    passwordLength,
+    useUppercase,
+    useLowercase,
+    useNumbers,
+    useSpecial
+  }
+}
+
+// Booleans on password suggestions
+function generatePassword() {
+  var passwordSuggestions = getSuggestions();
+  console.log(passwordSuggestions)
+  return printPassword(passwordSuggestions);
+}
+
+function printPassword(passwordSuggestions) {
+  if (!passwordSuggestions) {
+    return "Select valid password suggestions!"
+  }
+
+  var completeCharacterSet = [];
+  var chosenCharacterSet = [];
+  var finalCharaterSet = [];
+
+  if (passwordSuggestions.useUppercase) {
+    completeCharacterSet = completeCharacterSet.concat(passwordChoices.UpperCase);
+    chosenCharacterSet.push(randomPicker(passwordChoices.UpperCase));
+  }
+  if (passwordSuggestions.useLowercase) {
+    completeCharacterSet = completeCharacterSet.concat(passwordChoices.LowerCase);
+    chosenCharacterSet.push(randomPicker(passwordChoices.LowerCase));
+  }
+  if (passwordSuggestions.useNumbers) {
+    completeCharacterSet = completeCharacterSet.concat(passwordChoices.numbers);
+    chosenCharacterSet.push(randomPicker(passwordChoices.numbers));
+
+  }
+  if (passwordSuggestions.useSpecial) {
+    completeCharacterSet = completeCharacterSet.concat(passwordChoices.special);
+    chosenCharacterSet.push(randomPicker(passwordChoices.special));
+  }
+
+
+console.log(completeCharacterSet)
+console.log(chosenCharacterSet)
+
+  finalCharaterSet = finalCharaterSet.concat(chosenCharacterSet);
+
+  for (i=0; i < passwordSuggestions.passwordLength - chosenCharacterSet.length; i++) {
+    finalCharaterSet.push(randomPicker(completeCharacterSet));
+  }
+  console.log(finalCharaterSet.join(""));
+  return characterShuffle(finalCharaterSet.join(""));
+
+
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,21 +1,10 @@
-# 03 JavaScript: Password Generator
+# On Screen Interactive Password Generator
 
-## Your Task
-
-This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+## Description
+In our daily activities online, we come across pages where we are required to signup with username and password. One of the dangers of this is compromization of passwords over time. It is very essential that such information is kept secreet but that is a hard task. Setting up websites where the user gets a new password token generated verey single visit will ameliorate password compromises. To make it accessible, on screen approaches are ideal.
 
 ## Acceptance Criteria
-
+The following acceptance criteria was addopted from https://git.bootcampcontent.com/University-of-Toronto/UTOR-VIRT-FSF-PT-05-2023-U-LOLC/-/tree/main/03-JavaScript/02-Challenge
 ```
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
@@ -34,67 +23,15 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
 
-## Mock-Up
+## Method
+Starter codes for index.html, style.css and script.js were provided by Sean Roshan. The script.js file was editted to ensure that above acceptance criteria were met. Various funtions were created and are shown in the attached link for github below.
 
-The following image shows the web application's appearance and functionality:
+The github repository can be found at https://github.com/StanOp09/Project3
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+The live website is hosted at https://stanop09.github.io/Project3/
 
-## Grading Requirements
+## Appearance and Functionality
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+## Credits
+Sean Roshan
+ChatGPT
